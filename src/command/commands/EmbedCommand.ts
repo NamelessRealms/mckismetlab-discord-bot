d4bbba1aa06e164d1104ccde8f57789c0d88fa59
Quasi <quasipersonal58@gmail.com>
2022-06-29 14:00:46 +0800

    Update embed modpack

--- src/command/commands/EmbedCommand.ts
+++ src/command/commands/EmbedCommand.ts
@@ -85,17 +85,17 @@ export default class EmbedCommand extends SlashCommandBase {
 
     private _modpackEmbed(interaction: CommandInteraction<CacheType>): Array<MessageEmbed> {
 
-        const modpackName = "Project Ozone 3";
-        const modpackVersion = "3.4.6";
-        const modpackURL = "https://www.curseforge.com/minecraft/modpacks/project-ozone-3-a-new-way-forward";
-        const modpackImgURL = "https://media.forgecdn.net/avatars/thumbnails/153/427/64/64/636618242985977538.png";
+        const modpackName = "Enigmatica 2: Expert - Extended";
+        const modpackVersion = "0.61.1";
+        const modpackURL = "https://www.curseforge.com/minecraft/modpacks/enigmatica-2-expert-extended";
+        const modpackImgURL = "https://media.forgecdn.net/avatars/472/544/637762945960624722.png";
         const serverIP = "mckismetlab.net";
 
         const addedModsArray: Array<{ modName: string; modVersion: string, modURL: string }> = [
             {
                 modName: "Item Blacklist",
                 modVersion: "1.4.3",
-                modURL: "https://www.curseforge.com/minecraft/mc-mods/item-blacklist/download/2776296"
+                modURL: "https://www.curseforge.com/minecraft/mc-mods/item-blacklist/files/2776296"
             },
             {
                 modName: "DupeFix",
